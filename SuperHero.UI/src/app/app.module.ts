8ce4ab6c9092d6371c65b6c0218c5d7e801406b9
Novitskii_Kirill_Work <novitskii@pulsarm.ru>
2023-05-23 14:51:41 +0300

    Добавил модуль форм

--- SuperHero.UI/src/app/app.module.ts
+++ SuperHero.UI/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EditHeroComponent } from './components/edit-hero/edit-hero.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { EditHeroComponent } from './components/edit-hero/edit-hero.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-	HttpClientModule
+	HttpClientModule,
+	FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

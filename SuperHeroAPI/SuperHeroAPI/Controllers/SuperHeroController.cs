42cd580b1ff8768ce9322822c494e85ace0c5631
Novitskii_Kirill_Work <novitskii@pulsarm.ru>
2023-05-23 14:19:03 +0300

    Добавил controller для героев и метод оправки списка

---
+++ SuperHeroAPI/SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace SuperHeroAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class SuperHeroController : ControllerBase
+	{
+		[HttpGet]
+		public async Task<ActionResult<List<SuperHero>>> GetSuperHero()
+		{
+			return new List<SuperHero>
+			{
+				new SuperHero
+				{
+					Id = 1,
+					Name = "Spider Man",
+					FirstName = "Peter",
+					LastName = "Parker",
+					Place = "New York City"
+				},
+				new SuperHero
+				{
+					Id = 2,
+					Name = "Batman",
+					FirstName = "Bruce",
+					LastName = "Wayne",
+					Place = "Gotham City"
+				}
+			};
+		}
+	}
+}

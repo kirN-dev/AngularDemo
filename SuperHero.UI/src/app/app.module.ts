6f929668b9fb9fecb1f7e2bc107c5505af016a22
Novitskii_Kirill_Work <novitskii@pulsarm.ru>
2023-05-23 14:41:57 +0300

    Добавил компонент редактирования героя

--- SuperHero.UI/src/app/app.module.ts
+++ SuperHero.UI/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EditHeroComponent } from './components/edit-hero/edit-hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditHeroComponent
   ],
   imports: [
     BrowserModule,
